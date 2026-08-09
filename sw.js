7b027314df7c3c261abb8f7c83c0a4644a585681
marleyharris2033-ship-it <marleyharris2033@gmail.com>
2026-08-09 16:36:45 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
-const C="nextform-hotbar-fix-1",A=["./","index.html","styles.css","app.js","manifest.json","icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{let z=r.clone();caches.open(C).then(c=>c.put(e.request,z));return r}).catch(()=>caches.match(e.request))))
+self.addEventListener("install",e=>self.skipWaiting());
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
